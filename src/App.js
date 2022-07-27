80d78ac89008cc06249e268fae3724f892fed3ad
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-07-27 11:31:59 +0100

    set up link and route for addContacts

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ export default function App() {
       <nav>
         <h2>Menu</h2>
         <ul>
-          {/* TODO: Make these links */}
-          <li>Contacts List</li>
-          <li>Add New Contact</li>
+          <li><Link to='/'>Contacts List</Link></li>
+          <li><Link to='/contacts/add'>Add New Contact</Link></li>
         </ul>
       </nav>
       <main>
         <Routes>
           <Route path="/" element={ <ContactsList contacts={contacts}/>}/>
+          <Route path="/contacts/add" element={ <ContactsAdd contacts={contacts} setContacts={setContacts}/>}/>
         </Routes>
       </main>
     </>
