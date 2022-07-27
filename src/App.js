4739fcf8d2761984feb09af2c0aac3e600741e50
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-07-27 11:28:11 +0100

    imported contact list and showed first results

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import ContactsList from "./components/ContactsList"
 import ContactsAdd from "./components/ContactsAdd"
 import ContactsView from "./components/ContactsView"
 import "./styles/styles.css"
+const APIurl = 'http://localhost:4000/contacts'
 
 export default function App() {
   const [contacts, setContacts] = useState([])
   
   //TODO: Load all contacts on useEffect when component first renders
+
+  useEffect(() => {
+    fetch(APIurl)
+    .then(resp => resp.json())
+    .then(resp => {
+      console.log(resp)
+      setContacts(resp)})
+  },[])
 
   return (
     <>
@@ -22,7 +31,7 @@ export default function App() {
       </nav>
       <main>
         <Routes>
-          {/* TODO: Add routes here  */}
+          <Route path="/" element={ <ContactsList contacts={contacts}/>}/>
         </Routes>
       </main>
     </>
