183584d91ae5e8f13fe54822110937d46cc5e8c1
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-07-28 10:02:46 +0100

    loading spinner added

--- src/App.js
+++ src/App.js
@@ -4,41 +4,53 @@ import ContactsList from "./components/ContactsList"
 import ContactsAdd from "./components/ContactsAdd"
 import ContactsView from "./components/ContactsView"
 import { ContactsEdit } from "./components/ContactsEdit"
+import { Loading } from "./components/Loading"
 import "./styles/styles.css"
 import { APIurl } from "./utils/vars"
 
 export default function App() {
   const [contacts, setContacts] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   
   //TODO: Load all contacts on useEffect when component first renders
 
   useEffect(() => {
+    setIsLoading(true)
     fetch(APIurl)
     .then(resp => resp.json())
     .then(resp => {
       // console.log(resp)
-      setContacts(resp)})
+      setContacts(resp)
+      setIsLoading(false)
+    })
   },[])
+
 
   // console.log('contacts in app.js',contacts)
 
   return (
     <>
-      <nav>
-        <h2>Menu</h2>
-        <ul>
-          <li><Link to='/'>Contacts List</Link></li>
-          <li><Link to='/contacts/add'>Add New Contact</Link></li>
-        </ul>
-      </nav>
-      <main>
-        <Routes>
-          <Route path="/" element={ <ContactsList contacts={contacts} setContacts={setContacts}/>}/>
-          <Route path="/contacts/add" element={ <ContactsAdd contacts={contacts} setContacts={setContacts}/>}/>
-          <Route path='/contacts/:id' element={ <ContactsView />} />
-          <Route path="/contacts/:id/edit" element={<ContactsEdit contacts={contacts} setContacts={setContacts}/>} />
-        </Routes>
-      </main>
+      {(isLoading) ? (
+        <Loading />
+       ) : ( 
+      <>
+        <nav>
+          <h2>Menu</h2>
+            <ul>
+              <li><Link to='/'>Contacts List</Link></li>
+              <li><Link to='/contacts/add'>Add New Contact</Link></li>
+            </ul>
+        </nav>
+        <main>
+          <Routes>
+            <Route path="/" element={ <ContactsList contacts={contacts} setContacts={setContacts}/>}/>
+            <Route path="/contacts/add" element={ <ContactsAdd contacts={contacts} setContacts={setContacts}/>}/>
+            <Route path='/contacts/:id' element={ <ContactsView />} />
+            <Route path="/contacts/:id/edit" element={<ContactsEdit contacts={contacts} setContacts={setContacts}/>} />
+          </Routes>
+        </main>
+      </>
+      )}
     </>
   )
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import '../styles/loading.css'
+
+
+export const Loading = () => {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  )
+}
